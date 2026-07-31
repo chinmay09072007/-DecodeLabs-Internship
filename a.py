64a861a31196893ba72783d8e008c1b74fb91726
chinmay09072007 <jaiparas995@gmail.com>
2026-07-31 12:15:29 +0530

    Add files via upload

--- a.py
+++ a.py
@@ -1,17 +1,19 @@
 tasks=[]
 while True:
-    ch=int(input("Enter your choice:\n1. Add task\n2. View tasks\n3. Exit\n"))
+    ch=int(input("Enter your choice:\n1. Add task\n2. View tasks\n3. Delete task\n4. Exit\n"))
 
     if ch==1:
         print("Enter the task to be added:")
         task=input()
         tasks.append(task)
-        print(" task added successfully")
+        print(" task added successfully\n1")
     elif ch==2:    
-        print("Tasks to do")
-        for i,task  in enumerate(tasks, start=1):
-            print(f"{i}: {task}")
-        print("Tasks displayed successfully\n")    
+        if len(tasks) == 0:
+            print("No tasks available.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                print(f"{i}. {task}\n") 
+                   
     elif ch==3:
         print ("Enter task to be deleted")
         del_task=input()
